4725742f84bcd850a2b5699f47735ae216652db7
alanakov <angelikovalski@gmail.com>
2024-09-15 18:57:44 -0300

    adicionando as respostas das questoes da trilha 1

---
+++ src/Introducao.java
@@ -0,0 +1,73 @@
+package src;
+
+public class Introducao {
+    // 1 - A POO é um paradigma de programação com base no conceito de "objetos" que podem assumir dados na forma de campos, também conhecidos como propriedades de um código. Métodos e classes também são importantes na POO.
+    // Em relação ao papel das classes, assinale a resposta correta.
+
+    // B) É a partir das classes que a linguagem orientada a objetos se estrutura.
+
+
+
+
+    // 2 - Um objeto é instanciado por meio de uma classe. A diferença é que um objeto em Java é a entidade física e lógica, ao passo que uma classe em Java é considerada apenas como uma entidade lógica.
+    // Em relação à forma geral de uma classe, assinale a resposta correta.
+
+    // C) Uma classe é criada com a palavra-chave class.
+
+
+
+
+    // 3 - Um dos maiores desafios da POO é satisfazer o cliente, entregando um produto de qualidade. No entanto, a maior vantagem está na forma como suas características possibilitam minimizar a complexidade no desenvolvimento e na manutenção de software, aumentando a produtividade durante o processo de desenvolvimento.
+    // Os objetos são os elementos básicos da POO. Sobre os objetos e suas características, assinale a resposta correta.
+
+    // B) São compostos por estado e comportamento.
+
+
+
+
+    // 4 - Na linguagem de programação Java, a visibilidade padrão dos objetos, classes, atributos e métodos está limitada ao mesmo conjunto de elementos, ou seja, todos devem estar inseridos em um mesmo pacote. No código-fonte, a palavra-chave public modifica a visibilidade, excluindo as restrições e tornando o código mais amplo.
+    // Analise o código a seguir e selecione a afirmativa correta:
+    //
+    // public class Veiculo {
+    // private String placa;
+    // private String cor;
+    //
+    // private int anoFabricacao;
+    // public String getPlaca() {
+    // return placa;
+    // }
+    //
+    // public void setPlaca(String placa) {
+    // this.placa = placa;
+    // }
+    //
+    // public String getCor() {
+    // return cor;
+    // }
+    //
+    // public void setCor(String cor) {
+    // this.cor = cor;
+    // }
+    //
+    // public int getAnoFabricacao() {
+    // return anoFabricacao;
+    // }
+    //
+    // public void setAnoFabricacao(int anoFabricacao) {
+    // this.anoFabricacao = anoFabricacao;
+    // }
+    //
+    // }
+    //
+    // Selecione a resposta:
+
+    // B) As palavras-chave this em this.cor, this.placa e this.anoFrabricacao informam ao método que estamos nos referindo à variável de instância.
+
+
+
+
+    // 5 - A POO foi criada como uma alternativa para a programação estruturada, trazendo proximidade entre as "coisas" do mundo real e os objetos, criados no mundo virtual para refletir a realidade. Um objeto é algo genérico que pode definir qualquer coisa tangível.
+    //Quais são os componentes implementados para realizar algum tipo de tarefa?
+
+    // D) Métodos.
+}
